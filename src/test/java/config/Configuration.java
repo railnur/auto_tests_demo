31c6f4bd2be3dab13709fbdf891158ef457825da
Rail Nurullaev <rail.nurullaev@firstlinesoftware.com>
2017-10-06 17:33:33 +0300

    add config

--- src/test/java/config/Configuration.java
+++ src/test/java/config/Configuration.java
@@ -1,36 +1,63 @@
 package config;
 
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+
+import java.io.File;
+
 public class Configuration {
-    public static final String API_URI = "http://localhost:4444/v3";
-    public static  String SUBJECT_ID_RF = "00000000000456";
-    public static final String SUBJECT_ID_FOREIGN = "e579d5cb-47c9-431a-953b-74077f9f9ba9";
-    public static final String OWNER_ID = "00000000000467";
-    public static String CONSIGNEE_ID = "00000000000697";
-    public static String SELLER_ID = "00000000000464";
-    public static final String OP_DATE = "2017-08-12T14:00:00+03:00";
-    public static final String HS_CODE = "3004";
-    public static final String GTIN = "04606556002770";
-    public static final String BATCH = "20170912-1410";
-    public static final String EXP_DATE = "2020-02-02";
-    public static final int KIZ_COUNT = 10;
-    public static final int DOC_TYPE = 1;
-    public static final String DOC_NUM = "2017-09-12-14-01";
-    public static final String DOC_DATE = "2017-08-09";
-    public static final String INVOICE_DATE = "2017-08-01";
-    public static final String INVOICE_NUM = "123/07/2017";
-    public static final int CONTRACT_TYPE = 1;
-    public static final int CUSTOM_PROCEDURE_CODE = 4;
-    public static final String GTD_CUSTOMS_CODE = "12";
-    public static final String GTD_REG_DATE = "2017-08-01";
-    public static final String GTD_REG_NUMBER = "1234567";
-    public static final int ACCEPT_TYPE = 1;
-    public static final int SOURCE_TYPE = 1;
-    public static final int SHIPMENT_TYPE = 1;
-    public static final int RECEIVING_TYPE = 1;
-    public static final int ORDER_TYPE = 1;
-    public static int WITHDRAWAL_TYPE = 6;
-    public static final int CONTROL_TYPE = 1;
-    public static final int STORAGE_TYPE =1;
+
+    private Configurations configs = new Configurations();
+    private static PropertiesConfiguration config;
+
+    public Configuration(){
+        this.readConfig();
+    }
+
+    public static final String API_URI = config.getString("API_URI");
+    public static  String SUBJECT_ID_RF = config.getString("SUBJECT_ID_RF");
+    public static final String SUBJECT_ID_FOREIGN = config.getString("SUBJECT_ID_FOREIGN");
+    public static final String OWNER_ID = config.getString("OWNER_ID");
+    public static String CONSIGNEE_ID = config.getString("CONSIGNEE_ID");
+    public static String SELLER_ID = config.getString("SELLER_ID");
+    public static final String OP_DATE = config.getString("OP_DATE");
+    public static final String HS_CODE = config.getString("HS_CODE");
+    public static final String GTIN = config.getString("GTIN");
+    public static final String BATCH = config.getString("BATCH");
+    public static final String EXP_DATE = config.getString("EXP_DATE");
+    public static final int KIZ_COUNT = config.getInt("KIZ_COUNT");
+    public static final int DOC_TYPE = config.getInt("DOC_TYPE");
+    public static final String DOC_NUM = config.getString("DOC_NUM");
+    public static final String DOC_DATE = config.getString("DOC_DATE");
+    public static final String INVOICE_DATE = config.getString("INVOICE_DATE");
+    public static final String INVOICE_NUM = config.getString("INVOICE_NUM");
+    public static final int CONTRACT_TYPE = config.getInt("CONTRACT_TYPE");
+    public static final int CUSTOM_PROCEDURE_CODE = config.getInt("CUSTOM_PROCEDURE_CODE");
+    public static final String GTD_CUSTOMS_CODE = config.getString("GTD_CUSTOMS_CODE");
+    public static final String GTD_REG_DATE = config.getString("GTD_REG_DATE");
+    public static final String GTD_REG_NUMBER = config.getString("GTD_REG_NUMBER");
+    public static final int ACCEPT_TYPE = config.getInt("ACCEPT_TYPE");
+    public static final int SOURCE_TYPE = config.getInt("SOURCE_TYPE");
+    public static final int SHIPMENT_TYPE = config.getInt("SHIPMENT_TYPE");
+    public static final int RECEIVING_TYPE = config.getInt("RECEIVING_TYPE");
+    public static final int ORDER_TYPE = config.getInt("ORDER_TYPE");
+    public static int WITHDRAWAL_TYPE = config.getInt("WITHDRAWAL_TYPE");
+    public static final int CONTROL_TYPE = config.getInt("CONTROL_TYPE");
+    public static final int STORAGE_TYPE = config.getInt("STORAGE_TYPE");
     public static int[] SEQUENCE = {11, 12, 51};
+
+    private void readConfig(){
+
+        try
+        {
+            config = configs.properties(new File("config.properties"));
+
+        }
+        catch (ConfigurationException cex)
+        {
+            System.out.println(cex);
+        }
+    }
 
 }
